c14d42033e27d895342ce7531a637ea058659472
Naman Chauhan <29328764+chauhannaman98@users.noreply.github.com>
2019-04-15 11:08:10 +0530

    Create Factorial.asm

---
+++ Factorial.asm
@@ -0,0 +1,21 @@
+data segment
+   a dw 0005H
+   b dd ?
+data ends
+code segment
+assume cs:code, ds:data
+start:
+   mov ax, data
+   mov ds, ax
+   mov ax, 00
+   mov ax, a
+   L1:dec a
+      mul a
+      mov cx, a
+      mov cx, 01
+      jnz L1
+      mov word ptr c, ax
+      mov word ptr c+2, ax
+      int 3
+code ends
+end start
