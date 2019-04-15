9eec3eaf0278b12ff1183155a4cef129a48b0c62
Naman Chauhan <29328764+chauhannaman98@users.noreply.github.com>
2019-04-15 12:49:05 +0530

    Create Bubble Sort.asm

---
+++ Bubble Sort.asm
@@ -0,0 +1,25 @@
+data segment
+   arr db 09h, 72h, 10h, 80h, 98h
+   len equ $-arr
+data ends
+code segment
+assume cs:code, ds:data
+start:
+   mov ax, data
+   mov ds, ax
+   mov ch, len-1
+   L1:mov cl, ch
+      mov si, offset arr
+   L2:mov al, [si]
+      inc si
+      cmp al, [si]
+      jbe L3
+      xchg al, [si]
+      mov [si-1], al
+   L3:dec cl
+      jnz L2
+      dec ch
+      jnz L1
+      int 3
+code ends
+end start
