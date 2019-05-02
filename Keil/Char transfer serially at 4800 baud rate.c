e818c11572df569f80a67a87d7c06f0f928e97c9
Naman Chauhan <29328764+chauhannaman98@users.noreply.github.com>
2019-05-03 03:01:32 +0530

    Update Char transfer serially at 4800 baud rate.c

--- Keil/Char transfer serially at 4800 baud rate.c
+++ Keil/Char transfer serially at 4800 baud rate.c
@@ -2,13 +2,13 @@
 #include <reg51.h>
 
 void main(void)   {
-   TMOD=0X20;
-   TH1=0XFA;
-   SCON=0X50;
-   TR1=1;
+   TMOD=0X20;       //set timer 1 in Mode 2
+   TH1=0XFA;        //load baud rate 4800
+   SCON=0X50;       //set 8 data bit
+   TR1=1;           //start serial transmission
    while(1)   {
-      SBUF='A';
-      while(T1==0)
-      TI=0;
+      SBUF='A';     //load data to serial buffer register
+      while(TI==0); //wait until transmission complete
+      TI=0;         //clear transmission interrupt flag
       }
    }
