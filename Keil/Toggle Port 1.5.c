2ee757a6d93047b49366419e764e34f647273fe0
Naman Chauhan <29328764+chauhannaman98@users.noreply.github.com>
2019-05-03 09:36:52 +0530

    Update Toggle Port 1.5.c

--- Keil/Toggle Port 1.5.c
+++ Keil/Toggle Port 1.5.c
@@ -1,3 +1,13 @@
+//Both timers 0 and 1 use the same register, called TMOD (timer mode), 
+//to set the various timer operation modes in 8051 C programming. There are four operating modes of timer 0 and 1. 
+//To generate Time delay using timer registers:
+//Load the TMOD value register indicating which timer (timer 0 or timer 1) is to be used and which timer mode (0 or is selected
+//Load registers TL and TH with initial count value
+//Start the timer
+//Keep monitoring the timer flag (TF) until it rolls over from FFFFH to 0000.
+//After the timer reaches its limit and rolls over, in order to repeat the process - TH and TL must be reloaded with the original value, 
+//and TR is turned off by setting value to 0 and TF must be reloaded to 0.
+
 #include <reg51.h>
 
 void TOM1Delay(void);
