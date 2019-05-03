648f4f6fa51c14cf654d88600d5866a49a4782b9
Naman Chauhan <29328764+chauhannaman98@users.noreply.github.com>
2019-05-03 10:07:42 +0530

    Update Toggle Port 1.5.c

--- Keil/Toggle Port 1.5.c
+++ Keil/Toggle Port 1.5.c
@@ -22,10 +22,10 @@ void main(void)   {
 
 void TOM1Delay(void)   {
    TMOD=0x01;      //Timer Mode Control register, Timer 0 Mode 1
-   TLO=0xFD;       //load timer low byte
-   THO=0x4B;       //load timer high byte
-   TRO=1;          //turn on Timer0
-   while(TFO==1);  //wait for TF0 to roll back
-   TRO=0;          //turn off Timer0
-   TFO=0;          //clear TF0
+   TL0=0xFD;       //load timer low byte
+   TH0=0x4B;       //load timer high byte
+   TR0=1;          //turn on Timer0
+   while(TF0==1);  //wait for TF0 to roll back
+   TR0=0;          //turn off Timer0
+   TF0=0;          //clear TF0
 }
